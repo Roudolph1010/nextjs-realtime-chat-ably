6e77a00e36030f9be1e234c4591c2deaa5174414
Roudolph1010 <Roudolph1010@outlook.com>
2023-08-31 15:24:14 -0400

    Restructure for better component compartmentalization

--- app/page.js
+++ app/page.js
@@ -1,7 +1,33 @@
+import Head from 'next/head'
 import dynamic from 'next/dynamic';
 
-const IndexClient = dynamic(() => import('./page-client'), {
+const Chat = dynamic(() => import('../components/Chat'), {
   ssr: false,
 })
 
-export default IndexClient;
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Realtime Chat App with Ably, NextJS and Vercel</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="title">Next.js Chat Demo</h1>
+        <Chat />
+      </main>
+
+      <footer>
+        Powered by
+        <a href="https://vercel.com" target="_blank" rel="noopener noreferrer">
+          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+        </a>
+        and
+        <a href="https://ably.com" rel="noopener noreferrer">
+          <img src="/ably-logo.svg" alt="Ably Logo" className="logo ably" />
+        </a>
+      </footer>
+    </div>
+  )
+}
